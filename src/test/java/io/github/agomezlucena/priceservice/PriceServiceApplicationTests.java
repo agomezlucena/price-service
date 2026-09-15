e15b6db62e3f55f9f68ab61d1f57a4dd4a6e0694
Alejandro Gómez Lucena <alejandro_gomez_lucena@hotmail.com>
2026-09-15 18:14:22 +0200

    chore: created project scaffold.

---
+++ src/test/java/io/github/agomezlucena/priceservice/PriceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.agomezlucena.priceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
